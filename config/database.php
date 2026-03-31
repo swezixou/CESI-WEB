089316f1f7494144e75485b2029e19b147496006
Chakibzz <ayari897@gmail.com>
2026-03-31 18:45:46 +0200

    Add database configuration file

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+// ============================================================
+//  StageConnect – Configuration base de données
+// ============================================================
+
+define('DB_HOST',    'localhost');
+define('DB_PORT',    '3306');
+define('DB_NAME',    'stageconnect'); // le nom que tu as créé
+define('DB_USER',    'root');         // par défaut XAMPP
+define('DB_PASS',    '');             // par défaut vide sur XAMPP
+define('DB_CHARSET', 'utf8mb4');
